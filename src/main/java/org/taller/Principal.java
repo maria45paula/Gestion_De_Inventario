ecea54195c0b4f2d99e4b59719e7134c973f6e7a
yeymiT <yeymicuellarm@gmail.com>
2026-07-16 20:26:10 -0500

    Agregando interfaz de usuario

---
+++ src/main/java/org/taller/Principal.java
@@ -0,0 +1,10 @@
+package org.taller;
+
+import javax.swing.*;
+
+public class Principal {
+    private JLabel lblInicioSesion;
+    private JLabel lblContrasenia;
+    private JTextField textField1;
+    private JButton btnIngresar;
+}
